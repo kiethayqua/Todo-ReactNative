8314970e96788d385c53938c2194a1e6a13940e8
KietNguyen <kiethayqua@gmail.com>
2021-01-26 22:12:27 +0700

    format code and clean code

--- App.js
+++ App.js
@@ -1,44 +1,104 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Item from './Components/Item';
 
-export default class App extends React.Component{
-  constructor(props){
+export default class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       item: '',
       listItems: [],
-    }
+    };
   }
 
   addItem = () => {
-    var arr = [...this.state.listItems];
-    arr.push(this.state.item);
-    this.setState({listItems: arr});
-    this.setState({item: ''});
-  }
+    if (this.state.item === '') {
+      return;
+    }
+
+    this.setState({
+      listItems: [...this.state.listItems, this.state.item],
+      item: '',
+    });
+  };
 
   removeItem = (index) => {
-    var arr = [...this.state.listItems];
+    const arr = [...this.state.listItems];
     arr.splice(index, 1);
     this.setState({listItems: arr});
-  }
+  };
 
-  render(){
-    return(
-      <View style={{margin: 50}}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', borderBottomWidth: 1, paddingBottom: 20, borderBottomColor: '#ddd'}}>
-          <TextInput style={{flexGrow: 1, fontSize: 18}} placeholder="Enter your work" onChangeText={(value) => this.setState({item: value})}
-        value={this.state.item} onSubmitEditing={this.addItem}/>
-          <TouchableOpacity onPress={this.addItem}>
-            <Icon name="plus-circle" size={20} color='#88B04B'/>
-          </TouchableOpacity>
+  onChangeText = (value) => this.setState({item: value});
+
+  // renderItem co tham so la 1 obj
+  renderItem = ({item, index}) => {
+    // obj{item: item, index: index} --> object destructuring
+    return <Item title={item} removeItem={() => this.removeItem(index)}></Item>;
+  };
+
+  keyExtractor = (item, index) => {
+    return index.toString();
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.newItemWrapper}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Enter your work"
+              onChangeText={this.onChangeText}
+              value={this.state.item}
+              onSubmitEditing={this.addItem}
+            />
+            <TouchableOpacity onPress={this.addItem}>
+              <Icon name="plus-circle" size={20} color="#88B04B" />
+            </TouchableOpacity>
+          </View>
+          {/* {this.state.listItems.map((item, index) => (
+            <Item
+              title={item}
+              key={index}
+              removeItem={() => this.removeItem(index)}
+            />
+          ))} */}
+          <FlatList
+            data={this.state.listItems}
+            renderItem={this.renderItem}
+            keyExtractor={this.keyExtractor}
+          />
         </View>
-        {
-          this.state.listItems.map((item, index) => <Item title={item} key={index} removeItem={() => this.removeItem(index)}/>)
-        }
-      </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 50,
+  },
+
+  newItemWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    paddingBottom: 20,
+    borderBottomColor: '#ddd',
+  },
+
+  textInput: {
+    flexGrow: 1,
+    fontSize: 18,
+  },
+});
--- Components/Item.js
+++ Components/Item.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
-import {Text, Touchable, TouchableOpacity, View} from 'react-native';
-import  Icon  from 'react-native-vector-icons/FontAwesome';
+import {
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default class Item extends React.Component{
-    render(){
-        return(
-            <View style={{marginTop: 10, flexDirection: 'row', justifyContent: 'space-between'}}> 
-                <Text style={{fontSize: 20, fontWeight: '100'}}>{this.props.title}</Text>
-                <TouchableOpacity onPress={this.props.removeItem}>
-                    <Icon name="minus-circle" size={20} color='red'/> 
-                </TouchableOpacity>
-            </View>
-        );
-    }
+export default class Item extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{this.props.title}</Text>
+        <TouchableOpacity onPress={this.props.removeItem}>
+          <Icon name="minus-circle" size={20} color="red" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  text: {
+    fontSize: 20,
+    fontWeight: '100',
+  },
+});
